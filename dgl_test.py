1fb18483b22fb1ba67eef8927424011562917b3d
Tang <tangwj17@mails.tsinghua.edu.cn>
2020-08-09 21:34:14 +0800

    Initial commit

---
+++ dgl_test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@File : dgl_test.py    
+@Modify Time      @Author    @Version    @Desciption
+2020/8/9 15:17   TANG       1.0         test for the fundamental function of dgl
+"""
+import torch
+import dgl
+ 
