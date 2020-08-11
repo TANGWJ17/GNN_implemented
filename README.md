0f50c922454e6dd69b3ee9a388387ea7b6031a94
Tang <tangwj17@mails.tsinghua.edu.cn>
2020-08-11 08:25:01 +0800

    lately modify

--- README.md
+++ README.md
@@ -4,18 +4,19 @@ Code using GNN to predict instable units(node classification)
 
 ### Dependencies
 
-------
+
 
 * Python 3.7
 * Pytorch 1.5.0  cudatoolkit=10.2
 * ray-psops
 * dgl 0.4.3
+* pytorch-geometric 1.6.1
 
 Other versions may also work, but the ones listed are the ones I've used
 
 ### Data processing
 
-------
+
 
 Using ray-psops to batch generate samples
 
--- dgl_test.py
+++ dgl_test.py
@@ -7,5 +7,40 @@
 """
 import torch
 import dgl
+from dgl import DGLGraph
+from dgl.data import citation_graph as citation_graph
+import dgl
+import networkx as nx
+import matplotlib.pyplot as plt
+
 print(dgl.__version__)
- 
+
+def load_cora():
+    data = citation_graph.load_cora()
+    features = torch.from_numpy(data.features)
+    labels = torch.from_numpy(data.labels)
+    masks = torch.from_numpy(data.train_mask)
+    g = DGLGraph(data.graph)
+    return g, features, labels, masks
+
+
+if __name__ == '__main__':
+    # g, features, labels, masks = load_cora()
+    # print(features.type())
+    g = dgl.DGLGraph()
+    g.add_nodes(10)
+    # 逐条往图中添加边
+    for i in range(1, 4):
+        g.add_edge(i, 0)
+    # 批添加
+    src = list(range(5, 8))
+    dst = [0] * 3
+    g.add_edges(src, dst)
+
+    src = torch.tensor([8, 9])
+    dst = torch.tensor([0, 0])
+    g.add_edges(src, dst)
+
+    plt.figure(figsize=(14, 6))
+    nx.draw(g.to_networkx(), with_labels=True)
+    plt.savefig("Graph.png", format="PNG")
